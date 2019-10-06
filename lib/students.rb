ece23717369297af69d2db3591cf6563bc0b6bc4
Zach  Smith <zdsmith001@gmail.com>
2019-10-06 17:53:03 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,4 +1,4 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
   SELECT MAX(gpa) FROM students;
